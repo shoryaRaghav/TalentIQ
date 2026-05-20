77887d10bb5ffafaf449fd1257a6b7b910e17faf
shoryaxviii <raghav.shorya123@gmail.com>
2026-05-20 23:37:23 +0530

    db and tools setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+import {ENV} from "./env.js"
+
+
+export const connectDB=async()=>{
+    try{
+        const conn =await mongoose.connect(ENV.DB_URL)
+        console.log("Connected to Mongo DB",conn.connection.host)
+    }catch(error){
+        console.log("Error connecting to mongoDB")
+        process.exit(1)
+    }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path';
 import { ENV } from "./lib/env.js";
+import { connectDB } from './lib/db.js';
 
 const app=express();
 
@@ -30,8 +31,16 @@ if (ENV.NODE_ENV === "production") {
 //   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 // });
 
-app.listen(ENV.PORT,()=>{
-    console.log("Server is running on port:",ENV.PORT)
-})
 
 
+
+const startServer=async()=>{
+    try{
+        await connectDB();
+        app.listen(ENV.PORT,()=> console.log("Server is running on port:",ENV.PORT));
+    }catch(error){
+        console.log("Error starting the server");
+    }
+};
+
+startServer();
