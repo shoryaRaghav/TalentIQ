b1e15f8341402e839c9c89950a6a00ee222cd008
shoryaxviii <raghav.shorya123@gmail.com>
2026-05-21 01:32:02 +0530

    server.js app.get change

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,10 +20,10 @@ app.get("/health", (req, res) => {
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/{*path}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-} 
+}
 
 // app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
