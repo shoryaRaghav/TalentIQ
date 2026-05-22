15bd77d3ef6a3b00412abfa0b6d0f64b72d31bac
shoryaxviii <raghav.shorya123@gmail.com>
2026-05-22 15:05:54 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,25 +2,29 @@ import express from 'express'
 import path from 'path';
 import { ENV } from "./lib/env.js";
 import { connectDB } from './lib/db.js';
-// import cors from 'cors';
+import cors from 'cors';
+import {serve} from 'inngest/express';
+import {inngest,functions} from './lib/inngest.js';
 
 const app=express();
-
-// app.use(express.json());
-
 const __dirname = path.resolve();
+// //middleware
+app.use(express.json())
 
+
+//credentials :true meaning server allow a browser to include cookies on requests
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+
+app.use("/api/inngest", serve({client:inngest , functions}));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
 
 
-// //middleware
-// app.use(express.json())
 
-// //credentials :true meaning server allow a browser to include cookies on requests
-// app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
