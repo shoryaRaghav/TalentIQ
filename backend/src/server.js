4a07ff87397db7994a376932b86eceaad292ddca
shoryaxviii <raghav.shorya123@gmail.com>
2026-05-23 14:28:46 +0530

    session routes & controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,8 @@ import { clerkMiddleware } from '@clerk/express'
 
 import { protectRoute } from './middleware/protectRoute.js';
 import chatRoutes from './routes/chatRoutes.js';  
+import sessionRoutes from './routes/sessionRoutes.js';
+
 
 const app=express();
 const __dirname = path.resolve();
@@ -22,7 +24,9 @@ app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 app.use(clerkMiddleware()) // this adds auth field to ququest
 
 app.use("/api/inngest", serve({client:inngest , functions}));
-app.use("/api/chat",chatRoutes)
+app.use("/api/chat",chatRoutes);
+app.use("/api/sessions",sessionRoutes);
+
 
 
 app.get("/health", (req, res) => {
