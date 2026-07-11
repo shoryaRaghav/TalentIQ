bdd2bd479d18ca71d53bb77e9c6173e74010f4f1
shoryaRaghav <raghav.shorya123@gmail.com>
2026-07-11 16:13:47 +0530

    Protectroutes add to the sessionRoutes

--- backend/src/routes/sessionRoutes.js
+++ backend/src/routes/sessionRoutes.js
@@ -8,7 +8,7 @@ import { createSession, getActiveSessions, getMyRecentSessions, getSessionById, 
 const router =express.Router();
 
 router.post("/",protectRoute,createSession)
-router.get("/active",getActiveSessions)
+router.get("/active",protectRoute,getActiveSessions)
 router.get("/my-recent",protectRoute,getMyRecentSessions)
 
 router.get("/:id",protectRoute,getSessionById)
