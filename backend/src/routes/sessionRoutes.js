4a9ae8219c5d9260e9cce91b81b2fe49137d0962
shoryaRaghav <raghav.shorya123@gmail.com>
2026-07-11 16:47:42 +0530

    Add AWS S3 profile upload and ElastiCache Redis caching

--- backend/src/routes/sessionRoutes.js
+++ backend/src/routes/sessionRoutes.js
@@ -7,13 +7,12 @@ import { createSession, getActiveSessions, getMyRecentSessions, getSessionById, 
 
 const router =express.Router();
 
-router.post("/",protectRoute,createSession)
-router.get("/active",protectRoute,getActiveSessions)
-router.get("/my-recent",protectRoute,getMyRecentSessions)
+router.post("/", createSession);
+router.get("/active", getActiveSessions);
+router.get("/my-recent", getMyRecentSessions);
 
-router.get("/:id",protectRoute,getSessionById)
-router.post("/:id/join",protectRoute,joinSession)
-router.post("/:id/end",protectRoute,endSession)
-
+router.get("/:id", getSessionById);
+router.post("/:id/join", joinSession);
+router.post("/:id/end", endSession);
 
 export default router;
